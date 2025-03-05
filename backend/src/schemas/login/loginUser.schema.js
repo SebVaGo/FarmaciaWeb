26a0c7245cd72b1fe8926e3ed24c0b157bb3c45b
Sebastian <22sevago@gmail.com>
2025-03-04 23:26:20 -0500

    El login y creación de coockie funciona, ver algoritmo RS256 par implementación y ver middleware de validación de ip

--- backend/src/schemas/login/loginUser.schema.js
+++ backend/src/schemas/login/loginUser.schema.js
@@ -1,8 +1,22 @@
 const joi = require('joi');
 
 const loginUserSchema = joi.object({
-    email: joi.string().email().required(),
-    password: joi.string().required()
+    email: joi.string().trim().lowercase().email().max(100).required().messages({
+            'string.empty': "El correo es obligatorio.",
+            'any.required': "El correo es obligatorio.",
+            'string.email': "Debe ser un correo válido.",
+            'string.max': "No debe superar los 100 caracteres."
+        }),
+    password: joi.string().trim().min(8).max(50)
+        .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/)
+        .required()
+        .messages({
+            'string.empty': "La contraseña es obligatoria.",
+            'any.required': "La contraseña es obligatoria.",
+            'string.min': "Debe tener al menos 8 caracteres.",
+            'string.max': "No debe superar los 50 caracteres.",
+            'string.pattern.base': "Debe contener al menos una mayúscula, una minúscula, un número y un carácter especial."
+        })
 });
 
 module.exports = { loginUserSchema };
